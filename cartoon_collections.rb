558316add2354496498ffd11960376d12eed686f
Sean Good <tree662@protonmail.com>
2019-11-12 22:36:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array1.find do |cheese|
-    array2.include? == "cheese"
+  array.find do |cheese|
+    array.include? == "cheese"
   end
 end
