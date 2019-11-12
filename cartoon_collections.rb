511bc4342c77eba497e5a41651c9f9908cb0c7af
Sean Good <tree662@protonmail.com>
2019-11-12 22:55:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,8 @@ def long_planeteer_calls(array)
   # array.find{|word| word > 4}
 end
 
-def find_the_cheese(item)
-  array1.find do |item|
-    array2.include?(item)
+def find_the_cheese(array)
+  array.map do |item|
+    array.include?(item)
   end
 end
