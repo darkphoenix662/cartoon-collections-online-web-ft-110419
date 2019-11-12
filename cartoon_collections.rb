60d91a50d48a87fd9f5fbafc80a2b627500291af
Sean Good <tree662@protonmail.com>
2019-11-12 21:56:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find{|cheese| "cheese" == true}
+  array.find{|cheese| "cheese" = true}
   
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
